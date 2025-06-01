e5697ee161f8d9167b69edd15e8115df3a3928ef
J0hD3v <johanfievet@orange.fr>
2025-06-01 22:36:08 +0200

    ajout partenaires + filtres

--- sol-violette/script.js
+++ sol-violette/script.js
@@ -2,15 +2,22 @@ const menuLink = document.getElementsByClassName("menuLink");
 const sections = document.getElementsByClassName("expandable");
 
 for (i = 0; i < menuLink.length; i++) {
-  menuLink[i].addEventListener("click", function() {
+    menuLink[i].addEventListener("click", function() {
     const content = this.nextElementSibling;
+
     if (content.style.height === "50vh") {
+        // si ouvert -> ferme
         content.style.height = "0";
+        content.style.padding = "0 2rem";
     } else {
+        // si fermé -> ouvre
         for (const section of sections) {
+            // ferme les autres
             section.style.height = "0";
+            section.style.padding = "0 2rem";
         }
         content.style.height = "50vh";
+        content.style.padding = "2rem 2rem";
     }
   });
 }
