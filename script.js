cfc4ec22b9519c6e8b8a94a9775e7769877c89ce
J0hD3v <johanfievet@orange.fr>
2025-06-17 20:50:31 +0200

    ajout animation de connexion + nouveaux assets

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ for(let i=0; i<buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
         passwordTry.push(buttons[i].innerText);
         if (JSON.stringify(passwordTry)==JSON.stringify(password)) {
-            location.href = './sol-violette/solViolette.html';
+            playAnimation();
+            setTimeout(() => {
+                location.href = './sol-violette/solViolette.html';
+            }, 1500);
         }
         newMessage += "●";
         message.innerText = newMessage;
@@ -22,3 +25,41 @@ for(let i=0; i<buttons.length; i++) {
 }
 
 document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+
+function playAnimation() {
+    const section = document.createElement('section');
+    section.style = `
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+        height: 100vh;
+        background-color: rgba(0, 0, 0, 0.5);
+    `;
+    
+    const video = document.createElement('video');
+    video.setAttribute("autoplay",'');
+    video.setAttribute("loop",'');
+    video.setAttribute("muted",'');
+    video.setAttribute("disablePictureInPicture",'');
+    video.setAttribute("playsinline",'');
+    video.style = "width: 70vw";
+    
+    const source = document.createElement('source');
+    source.src = "./assets/Croix-ezgif.com-gif-to-webm-converter.webm";
+    source.type = "video/webm";
+    
+    document.body.appendChild(section);
+    section.appendChild(video);
+    video.appendChild(source);
+    
+    
+    video.addEventListener('canplay', () => {
+        video.play().catch(err => {
+            console.warn("Autoplay failed:", err);
+        });
+    });
+}
